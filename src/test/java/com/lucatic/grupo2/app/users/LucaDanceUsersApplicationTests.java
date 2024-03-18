b66c5b9712563a10d56c61de3db69dbc2153d7b3
jgp14 <jaimegarciaparada@gmail.com>
2024-03-18 12:27:23 +0100

    upload project users

---
+++ src/test/java/com/lucatic/grupo2/app/users/LucaDanceUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucatic.grupo2.app.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucaDanceUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
