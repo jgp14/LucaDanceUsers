36ee343a68a41489e7979df69b75ec3e14c26f42
Ruben Grueso Vega <ruben.gve@gmail.com>
2024-03-18 13:53:05 +0100

    aaa

--- src/main/java/com/lucatic/grupo2/app/users/LucaDanceUsersApplication.java
+++ src/main/java/com/lucatic/grupo2/app/users/LucaDanceUsersApplication.java
@@ -2,8 +2,10 @@ package com.lucatic.grupo2.app.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class LucaDanceUsersApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/lucatic/grupo2/app/users/models/User.java
@@ -0,0 +1,23 @@
+package com.lucatic.grupo2.app.users.models;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(name = "last_name")
+    private String lastName;
+    private String email;
+    private String password;
+    @Column(name = "register_date")
+    private LocalDateTime registerDate;
+
+}
